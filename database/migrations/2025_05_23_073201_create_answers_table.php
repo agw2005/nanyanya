34fe899809b97227ffee7123562fb08555f5805a
agw2005 <danial.agw2005@gmail.com>
2025-06-03 20:25:08 +0700

    fix(answers_table): fixed sqlite to postgresql compatibility issues with contrained keys

--- database/migrations/2025_05_23_073201_create_answers_table.php
+++ database/migrations/2025_05_23_073201_create_answers_table.php
@@ -13,14 +13,23 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade')->index();
-            $table->foreignId('quiz_id')->constrained()->onDelete('cascade')->index();
-            $table->foreignId('question_id')->constrained()->onDelete('cascade')->index();
-            $table->string('selected_option')->nullable(); // 'A', 'B', 'C', etc.
+
+            $table->foreignId('user_id')->index();
+            $table->foreign('user_id', 'fk_answers_user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->foreignId('quiz_id')->index();
+            $table->foreign('quiz_id', 'fk_answers_quiz_id')->references('id')->on('quizzes')->onDelete('cascade');
+
+            $table->foreignId('question_id')->index();
+            $table->foreign('question_id', 'fk_answers_question_id')->references('id')->on('questions')->onDelete('cascade');
+
+            $table->string('selected_option')->nullable();
             $table->timestamps();
-            $table->unique(['user_id', 'question_id']); // Prevent multiple answers per question by one user
+
+            $table->unique(['user_id', 'question_id']);
         });
     }
+
 
     /**
      * Reverse the migrations.
